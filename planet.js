4d5c53163edac2fec24b9c4ed3c6eb0cb11f9778
shou241 <73938618+shou241@users.noreply.github.com>
2021-09-05 21:49:41 +0530

    Add files via upload

---
+++ planet.js
@@ -0,0 +1,44 @@
+function mercury()
+{
+window.location="mercury.html"    
+}
+
+function venus()
+{
+window.location="venus.html"    
+}
+
+function earth()
+{
+window.location="earth.html"    
+}
+
+function mars()
+{
+window.location="mars.html"    
+}
+
+function jupiter()
+{
+window.location="jupiter.html"    
+}
+
+function saturn()
+{
+window.location="saturn.html"    
+}
+
+function uranus()
+{
+window.location="uranus.html"    
+}
+
+function neptune()
+{
+window.location="neptune.html"    
+}
+
+function positive_vibes()
+{
+    window.location="vibes.html"     
+}
